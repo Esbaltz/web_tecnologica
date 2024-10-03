bfa4b2fc5dd5c658a83b5fedaf4a1c90b197883d
Elías <el.antican@duocuc.cl>
2024-10-03 17:07:29 -0300

    detalles

--- web/views.py
+++ web/views.py
@@ -6,6 +6,19 @@ from .models import Computadora
 def login (request):
     return render(request, 'index.html')
 
-def tienda (request):
-    computadores = Computadora.objects.all()
+def tienda(request):
+    computadores = Computadora.objects.all()  
+
+    # Filtrar por rango de precio
+    precio_minimo = request.GET.get('precio_minimo')
+    precio_maximo = request.GET.get('precio_maximo')
+    
+    if precio_minimo and precio_maximo:
+        computadores = computadores.filter(precio__gte=precio_minimo, precio__lte=precio_maximo)
+
+    # Filtrar por marcas
+    marcas_seleccionadas = request.GET.getlist('marcas')
+    if marcas_seleccionadas:
+        computadores = computadores.filter(marca__in=marcas_seleccionadas)
+
     return render(request, 'tienda.html', {'computadores': computadores})
